88cd9ec1628d3b9f3681c0ad30711b885cde1827
Chu-Cheng Lin <chu.cheng.lin@gmail.com>
2015-04-08 16:34:28 -0400

    partial

--- toy.py
+++ toy.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python
 __author__ = 'as1986'
 
-import theano as T
+import theano.tensor as T
+import theano
 import numpy as np
-from deeplearning import *
+import deeplearning.layer
+import deeplearning.learning_rule
+import deeplearning.utils
 from io import open
 
 def load_bitext(fname, using_vocab_l = None, using_vocab_r = None):
@@ -26,7 +29,7 @@ def load_bitext(fname, using_vocab_l = None, using_vocab_r = None):
     return dict_l, dict_r, sens_l, sens_r
 
 
-def load_sentences(sentences, using_vocab = None):
+def load_sentences(sentences, using_vocab = None, embedding=None):
     ''' loads a space separated file, creates the vocab dict, and return arrays of sentences
     :param fname: file name
     :return: (vocab, sentences)
@@ -43,7 +46,10 @@ def load_sentences(sentences, using_vocab = None):
         for w in l.strip().split(u' '):
             if w not in dict_to_return:
                 dict_to_return[w] = len(dict_to_return) + 1
-            this_line.append(dict_to_return[w])
+            if embedding is None:
+                this_line.append(dict_to_return[w])
+            else:
+                this_line.append(np.asarray(embedding[w.lower()], dtype=np.float32))
         list_to_return.append(this_line)
     return dict_to_return, list_to_return
 
@@ -54,8 +60,35 @@ def main():
     args = parser.parse_args()
     (vocab_l, _, contents_l,_) = load_bitext(args.textfile)
     print 'vocab size: {}'.format(len(vocab_l))
-    for content in contents_l:
-        print u' '.join([unicode(x) for x in content])
+
+    n_word = len(vocab_l)
+    e_dim = 100
+    x = T.imatrix('x')
+    t = T.imatrix('t')
+    layers = [
+        deeplearning.layer.tProjection(n_word, e_dim),
+        deeplearning.layer.LSTM(e_dim, 100, minibatch=True),
+        deeplearning.layer.LSTM(100, 100, minibatch=True)
+    ]
+
+    for i, layer in enumerate(layers):
+        if i == 0:
+            layer_out = layer.fprop(x)
+        else:
+            layer_out = layer.fprop(layer_out)
+    params = []
+    for layer in layers:
+        params += layer.params
+    y = layers[-1].h[-1]
+    y_func = theano.function([x], y)
+    eval_y = y_func([[1, 2, 3, 4, 5, 6]])
+    # eval_y_2 = y_func([[1, 2, 3, 4, 5]])
+    cost = ((eval_y - y) ** 2 ).sum()
+    cost_func = theano.function([x], cost)
+    print cost_func([[1,2,3,4,5]])
+    updates = learning_rule(cost, params, eps=1e-6, rho=0.65, clip=1., method="adadelta")
+
+
     return
 
 if __name__ == '__main__':
